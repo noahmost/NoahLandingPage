31160f9def1498d332c95e38cb76fb849b51ba9b
Noah Mostacero <noahmostacero@gmail.com>
2015-05-21 10:52:21 -0700

    Push test

--- index.php
+++ index.php
@@ -7,11 +7,27 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <link type="text/css" rel="stylesheet" href="main.css">
+        <link type="text/css" rel="stylesheet" href="bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="bootstrap.css">
+        <title>Noah Landing Page</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div>
+        <li>
+                <a href="index.html" style="text-decoration: none;" id="button1">
+                    Home
+                </a>
+                <a href="aboutme.html" style="text-decoration: none;" id="button2">
+                    Projects
+                </a>
+<!--                        <a href="hobbies.html" style="text-decoration: none;" id="button3">
+                            Hobbies
+                        </a>-->
+                        </li>
+        </div>
     </body>
 </html>
+<?php
+
+?>
