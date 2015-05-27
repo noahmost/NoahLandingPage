32e475f6902eba720dfd87ba7ea787318aa0ce60
Noah Mostacero <noahmostacero@gmail.com>
2015-05-27 11:02:24 -0700

    Created layouts for all pages

--- projects.php
+++ projects.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -17,19 +11,21 @@ and open the template in the editor.
             <p>container</p>
             <div class='row'>
                 <p>row</p>
-                <div class='col-xs-12 name'>Noah Mostacero</div>
-                </div>
-<!--                <div class='col-xs-12'><p>column 12 line</p></div>-->
-            <div class='row'>
+                <div class='col-xs-12 name'>Projects column 12</div>
+                <div class='col-xs-12'><p>column 12 line</p></div>
                 <div class='col-xs-4'><p><a href='index.php'>column 4 Index Link</a></p></div>
                 <div class='col-xs-4'><p><a href='projects.php'>column 4 Project Link</a></p></div>
                 <div class='col-xs-4'><p><a href='contact.php'>column 4 Contact Link</a></p></div>
             </div>
-<!--            <div class='row'>
+           <div class='row'>
                 <p>row</p>
-                <div class='col-xs-9'> column 9</div>
-                <div class='col-xs-3'> column 3</div>
-            </div>-->
+                <div class='col-xs-8'> column 8 picture of project link</div>
+                <div class='col-xs-4'> column 4 text explaining project</div>
+            </div>
+            <div class='row'>
+                <div class='col-xs-8'>column 8</div>
+                <div class='col-xs-4'>column 4</div>
+            </div>
             
         </div>
     </body>
