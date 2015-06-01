00bc6d62f4a65e6bbd315ef6d7ecab5f2302c432
Noah Mostacero <noahmostacero@gmail.com>
2015-06-01 11:56:36 -0700

    added css for index.php

--- index.php
+++ index.php
@@ -7,23 +7,19 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <link type="text/css" rel="stylesheet" href="main.css">
-        <link type="text/css" rel="stylesheet" href="bootstrap.min.css">
-        <link type="text/css" rel="stylesheet" href="bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="index.css">
+<!--        <link type="text/css" rel="stylesheet" href="bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="bootstrap.css">-->
         <title>Noah Landing Page</title>
     </head>
     <body>
-        <div class='container'>
-            <p>container</p>
-            <div class='row'>
-                <p>row</p>
-                <div class='col-xs-12 name'>Noah Mostacero</div>
-                </div>
-<!--                <div class='col-xs-12'><p>column 12 line</p></div>-->
-            <div class='row'>
-                <div class='col-xs-4'><p><a href='index.php'>column 4 Index Link</a></p></div>
-                <div class='col-xs-4'><p><a href='projects.php'>column 4 Project Link</a></p></div>
-                <div class='col-xs-4'><p><a href='contact.php'>column 4 Contact Link</a></p></div>
+<!--        <div class="blackbox">-->
+        <div class="namebox">Noah Mostacero</div>
+        <div class="line"></div>
+        <div class="links"><a href="index.php">Home</a> <a href="projects.php">Projects</a> <a href="contact.php">Contact Me</a></div>
+<!--        </div>-->
+    </body>
+<!--                
             </div>
 <!--            <div class='row'>
                 <p>row</p>
